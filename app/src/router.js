fcd4443e9bab5780b0e7545e91bd79f9de7aabc4
avanvalk <drewvv@gmail.com>
2018-12-10 15:16:09 -0800

    fixed router problem, edit function getting closer

--- app/src/router.js
+++ app/src/router.js
@@ -1,11 +1,14 @@
 import VueRouter from 'vue-router';
 import Home from './components/home/Home.vue';
 import Students from './components/students/Students.vue';
+import StudentDetail from './components/students/StudentDetail';
+
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/students', component: Students },
-    { path: '*', redirect: '/' },        
+    { path: '/students/:id', component: StudentDetail },
+    { path: '*', redirect: '/' }        
   ]
 });
